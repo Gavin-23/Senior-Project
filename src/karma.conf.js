bfcfa826311c58a66678fe0afc6f3972f2d1ecd6
Gavin <zihaoyu23@gmail.com>
2019-07-23 13:13:03 +0800

    fix bug

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,11 +4,10 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular', 'requirejs'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     
     plugins: [
       require('karma-jasmine'),
-      require('karma-requirejs'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
